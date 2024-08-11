1d2f2adaff9ab105e1e5b926695b4323ecdbaced
Eugene <compserv@yahoo.com>
2024-08-11 11:26:47 +0300

    fix bug

--- main unit.java
+++ main unit.java
@@ -1,6 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        if(args[2].equals("name")) throw IlligalArgumentExceptional("Illigal argument");
+        if(args[1].equals("name")) throw IlligalArgumentExceptional("Illigal argument");
     }
 }
