6e239349ef03cbcedff227bef23c49ddd955224b
Eugene <compserv@yahoo.com>
2024-08-11 11:28:34 +0300

    add print messaage to log

--- main unit.java
+++ main unit.java
@@ -2,5 +2,6 @@
 public class Main {
     public static void main(String[] args) {
         if(args[1].equals("name")) throw IlligalArgumentExceptional("Illigal argument");
+		System.out.println("go on!");
     }
 }
